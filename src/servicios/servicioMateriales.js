695f458e4e0382b3655e90a6a6271785d169432f
Jose Luis Tamara <jose.tamara426@pascualbravo.edu.co>
2025-01-17 18:40:12 -0500

    Implementación de los métodos listar y eliminar materiales: controlador, ruta y servicio adaptados para gestionar estas operaciones con Google Classroom.

--- src/servicios/servicioMateriales.js
+++ src/servicios/servicioMateriales.js
@@ -15,7 +15,40 @@ const crearMaterial = async (courseId, courseWorkMaterial) => {
     }
 };
 
+// listar material
+const listarMaterial = async (courseId) => {
+    const classroom = google.classroom({ version: 'v1', auth: oauth2Client });
+    try {
+        // solicitud para obtener los materiales
+        const res = await classroom.courses.courseWorkMaterials.list({
+            courseId,
+        });
+        return res.data.courseWorkMaterials; // devuelve los materiales listados
+    } catch (error) {
+        console.log('Error al listar los materiales', error.message);
+        throw new Error(error.message);
+    }
+};
+
+// Eliminar material
+const eliminarMaterial = async (courseId, tareaId) => {
+    const classroom = google.classroom({ version: 'v1', auth: oauth2Client });
+    
+    try {
+        await classroom.courses.courseWorkMaterials.delete({
+            courseId,
+            id: materialId,
+        });
+        console.log(`Material ${materialId} eliminado correctamente del curso ${courseId}`);
+    } catch (error) {
+        console.log(`Error al eliminar el material ${materialId} en el curso ${courseId}: ${error.message}`);
+        throw new Error(`Error al eliminar el material: ${error.message}`);
+    }
+};
+
 module.exports = {
-    crearMaterial
+    crearMaterial,
+    listarMaterial,
+    eliminarMaterial
 
 };
